f296e4aeaa58b598f5805e384f252a0ec48407c4
Kevin Nguyen <knth@hey.com>
2023-12-01 14:35:03 +0100

    feat: day01-part1

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -5,7 +5,36 @@ fn main() {
 }
 
 fn part1(input: &str) -> String {
-    "todo!()".to_string()
+    let numbers: Vec<_> = (0..=9)
+        .collect::<Vec<i32>>()
+        .iter()
+        .map(|n| n.to_string())
+        .collect();
+
+    let lines = input.lines();
+
+    let res = lines
+        .map(|l| {
+            let first_digit = l
+                .chars()
+                .find(|c| numbers.contains(&c.to_string()))
+                .unwrap()
+                .to_string();
+
+            let second_digit = l
+                .chars()
+                .rev()
+                .find(|c| numbers.contains(&c.to_string()))
+                .unwrap()
+                .to_string();
+
+            let digits = format!("{first_digit}{second_digit}");
+
+            digits.parse::<i32>().unwrap()
+        })
+        .sum::<i32>();
+
+    res.to_string()
 }
 
 #[cfg(test)]
@@ -14,7 +43,13 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = part1("");
-        assert_eq!(result, "4".to_string());
+        let result = part1(
+            "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet",
+        );
+
+        assert_eq!(result, "142".to_string());
     }
 }
