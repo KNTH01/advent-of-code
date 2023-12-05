50dd3b334381758b8be15693847a96257298aefe
Kevin Nguyen <knth@hey.com>
2023-12-05 22:22:49 +0100

    feat: papaparse1

--- 2023/day-05/src/bin/part1.rs
+++ 2023/day-05/src/bin/part1.rs
@@ -1,15 +1,20 @@
+use day_05::parser::parse;
+
 fn main() {
-    let input = include_str!("./input1.txt");
+    let input = include_str!("./input.txt");
     let output = process(input);
     dbg!(&output);
 }
 
 fn process(input: &str) -> String {
-    let res = input;
+    let res = parse(input).unwrap();
 
-    // je fais mon algo blablabla
+    let (seeds, maps) = res;
 
-    res.to_string()
+    dbg!(&seeds);
+    dbg!(&maps);
+
+    "0".to_string()
 }
 
 #[cfg(test)]
@@ -19,12 +24,41 @@ mod tests {
     #[test]
     fn it_works() {
         let result = process(
-            "1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet",
+            "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4",
         );
 
-        assert_eq!(result, "142".to_string());
+        assert_eq!(result, "13".to_string());
     }
 }
---
+++ 2023/day-05/src/parser.rs
@@ -0,0 +1,61 @@
+use nom::bytes::complete::{tag, take_until};
+use nom::character::complete::{digit1, line_ending, space1};
+use nom::combinator::map_res;
+use nom::multi::{many1, separated_list1};
+use nom::sequence::{preceded, tuple};
+use nom::IResult;
+use std::error::Error;
+
+type Seeds = Vec<u32>;
+
+type DestSrcRng = (u32, u32, u32);
+type Map = Vec<DestSrcRng>;
+type Maps = Vec<Vec<DestSrcRng>>;
+
+fn parse_u32(input: &str) -> IResult<&str, u32> {
+    map_res(digit1, |digit_str: &str| digit_str.parse::<u32>())(input)
+}
+
+fn parse_map(input: &str) -> IResult<&str, Vec<DestSrcRng>> {
+    let (input, _) = take_until(":")(input)?;
+
+    let (input, _) = tag(":\n")(input)?;
+
+    let (input, map_items) = many1(tuple((
+        parse_u32,
+        tag(" "),
+        parse_u32,
+        tag(" "),
+        parse_u32,
+        line_ending,
+    )))(input)?;
+
+    Ok((
+        input,
+        map_items
+            .into_iter()
+            .map(|(a, _, b, _, c, _)| (a, b, c))
+            .collect(),
+    ))
+}
+
+fn parse_maps(input: &str) -> IResult<&str, Maps> {
+    many1(parse_map)(input)
+}
+
+fn parse_seeds(input: &str) -> IResult<&str, Seeds> {
+    preceded(tag("seeds: "), separated_list1(space1, parse_u32))(input)
+}
+
+fn main_parser(input: &str) -> IResult<&str, (Seeds, Maps)> {
+    let (input, seeds) = parse_seeds(input)?;
+    let (input, maps) = parse_maps(input)?;
+
+    Ok((input, (seeds, maps)))
+}
+
+pub fn parse(input: &str) -> Result<(Seeds, Maps), Box<dyn Error>> {
+    let (_, res) = main_parser(input).expect("should be able to parse");
+
+    Ok(res)
+}
