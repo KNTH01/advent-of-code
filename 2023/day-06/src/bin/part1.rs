00356865cfe5cad8ea991f75adf06d11c2daa777
Kevin Nguyen <knth@hey.com>
2023-12-06 21:23:32 +0100

    feat: part 1

--- 2023/day-06/src/bin/part1.rs
+++ 2023/day-06/src/bin/part1.rs
@@ -10,7 +10,7 @@ use nom::{
 use nom_supreme::ParserExt;
 
 fn main() {
-    let input = include_str!("./input.txt");
+    let input = include_str!("./input1.txt");
     let output = process(input);
 
     dbg!(&output);
