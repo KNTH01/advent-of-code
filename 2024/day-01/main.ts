ea451f00022f3161120774f8ea64bdc99d6f1949
Kevin Nguyen <knth@hey.com>
2024-12-01 18:03:15 +0100

    feat: day one

--- 2024/day-01/main.ts
+++ 2024/day-01/main.ts
@@ -1,8 +1,24 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import { zip } from "@std/collections/zip";
+
+export async function main() {
+  const text = await Deno.readTextFile("input.txt");
+  const lines = text.split("\n");
+
+  const input = lines
+    .filter(Boolean)
+    .map((text) => text.split(/\s+/).map(Number));
+
+  const listLeft = input.map((pair) => pair[0]).sort();
+  const listRight = input.map((pair) => pair[1]).sort();
+
+  return zip(listLeft, listRight)
+    .map(([left, right]) => Math.abs(left - right))
+    .reduce((acc, curr) => acc + curr, 0);
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  const res = await main();
+
+  console.log("[LS] -> main.ts:21 -> res: ", res);
 }
