b35179b69a67f7cee999c21a243c0168f43cdba6
Kevin Nguyen <knth@hey.com>
2024-12-02 13:42:29 +0100

    feat: day-02 part2 completed

--- 2024/day-02/main.ts
+++ 2024/day-02/main.ts
@@ -1,75 +1,27 @@
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  const inputFile = "input-base.txt";
-  // const inputFile = "input-1.txt";
+  // const inputFile = "input-base.txt";
+  const inputFile = "input-1.txt";
   const text = await Deno.readTextFile(inputFile);
   const reports = text
     .split("\n")
     .filter(Boolean)
     .map((textReport) => textReport.split(" ").map(Number));
 
-  const resPart1 = reports.filter((report) => {
-    let isIncreasing = false;
-
-    for (let i = 0; i < report.length; i++) {
-      const a = report[i];
-      const b = report[i + 1];
-
-      if (i === 0 && a < b) {
-        isIncreasing = true;
-      }
-
-      if (i + 1 < report.length) {
-        if (
-          (i > 0 && ((a < b && !isIncreasing) || (a > b && isIncreasing))) ||
-          !isInRange(a, b)
-        ) {
-          return false;
-        }
-      }
-    }
-    return true;
-  }).length;
+  const resPart1 = reports.filter((report) => isSafe(report)).length;
   console.log("[LS] -> main.ts:11 -> restPart1: ", resPart1);
 
   const resPart2 = reports.filter((report) => {
-    let isIncreasing = false;
-    let joker = null;
+    const isToleratedUnsafe = [...Array(report.length).keys()]
+      .map((index) => {
+        const testingReport = [...report];
+        testingReport.splice(index, 1);
 
-    for (let i = 0; i < report.length; i++) {
-      const a = report[i];
-      let b = report[i + 1];
+        return !isSafe(testingReport);
+      })
+      .every((unsafe) => unsafe === true);
 
-      if (i === 0 && a < b) {
-        // TODO:
-        isIncreasing = true;
-      }
-
-      if (i + 1 < report.length) {
-        if (
-          (i > 0 && ((a < b && !isIncreasing) || (a > b && isIncreasing))) ||
-          !isInRange(a, b)
-        ) {
-          if (joker) {
-            return false;
-          }
-
-          b = report[i + 2];
-
-          if (
-            (a < b && !isIncreasing) ||
-            (a > b && isIncreasing) ||
-            !isInRange(a, b)
-          ) {
-            return false;
-          }
-
-          i += 2;
-          joker = true;
-        }
-      }
-    }
-    return true;
+    return isSafe(report) || !isToleratedUnsafe;
   }).length;
   console.log("[LS] -> main.ts:66 -> resPart2: ", resPart2);
 }
@@ -77,4 +29,53 @@ if (import.meta.main) {
 function isInRange(a: number, b: number) {
   const range = [1, 2, 3];
   return range.includes(Math.abs(a - b));
+}
+
+function isSafe(report: number[]) {
+  const swapCountAscending = findSwaps(
+    report.map((val, index) => [index, val]).sort((a, b) => a[1] - b[1]),
+  );
+  const swapCountDescending = findSwaps(
+    report.map((val, index) => [index, val]).sort((a, b) => b[1] - a[1]),
+  );
+
+  if (swapCountAscending > 0 && swapCountDescending > 0) {
+    return false;
+  }
+
+  for (let i = 0; i < report.length; i++) {
+    if (i + 1 < report.length) {
+      const a = report[i];
+      const b = report[i + 1];
+
+      if (!isInRange(a, b)) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+function findSwaps(arrPos: number[][]): number {
+  const n = arrPos.length;
+  let swaps = 0;
+  const visited = new Array(n).fill(false);
+
+  for (let i = 0; i < n; i++) {
+    if (visited[i] || arrPos[i][0] === i) continue;
+
+    let cycleSize = 0;
+    let currentPos = i;
+
+    while (!visited[currentPos]) {
+      visited[currentPos] = true;
+      currentPos = arrPos[currentPos][0];
+      cycleSize++;
+    }
+
+    swaps += cycleSize - 1;
+  }
+
+  return swaps;
 }
