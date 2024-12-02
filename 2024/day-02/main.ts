da6097f19e65be399ee5e1f80e6f50409d8e43d3
Kevin Nguyen <knth@hey.com>
2024-12-02 10:59:57 +0100

    feat: part2 for base input

--- 2024/day-02/main.ts
+++ 2024/day-02/main.ts
@@ -1,21 +1,19 @@
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  // const inputFile = "input-base.txt";
-  const inputFile = "input-1.txt";
+  const inputFile = "input-base.txt";
+  // const inputFile = "input-1.txt";
   const text = await Deno.readTextFile(inputFile);
   const reports = text
     .split("\n")
     .filter(Boolean)
     .map((textReport) => textReport.split(" ").map(Number));
 
-  const range = [1, 2, 3];
   const resPart1 = reports.filter((report) => {
     let isIncreasing = false;
 
     for (let i = 0; i < report.length; i++) {
       const a = report[i];
       const b = report[i + 1];
-      const isInRange = range.includes(Math.abs(a - b));
 
       if (i === 0 && a < b) {
         isIncreasing = true;
@@ -24,7 +22,7 @@ if (import.meta.main) {
       if (i + 1 < report.length) {
         if (
           (i > 0 && ((a < b && !isIncreasing) || (a > b && isIncreasing))) ||
-          !isInRange
+          !isInRange(a, b)
         ) {
           return false;
         }
@@ -32,6 +30,51 @@ if (import.meta.main) {
     }
     return true;
   }).length;
-
   console.log("[LS] -> main.ts:11 -> restPart1: ", resPart1);
+
+  const resPart2 = reports.filter((report) => {
+    let isIncreasing = false;
+    let joker = null;
+
+    for (let i = 0; i < report.length; i++) {
+      const a = report[i];
+      let b = report[i + 1];
+
+      if (i === 0 && a < b) {
+        // TODO:
+        isIncreasing = true;
+      }
+
+      if (i + 1 < report.length) {
+        if (
+          (i > 0 && ((a < b && !isIncreasing) || (a > b && isIncreasing))) ||
+          !isInRange(a, b)
+        ) {
+          if (joker) {
+            return false;
+          }
+
+          b = report[i + 2];
+
+          if (
+            (a < b && !isIncreasing) ||
+            (a > b && isIncreasing) ||
+            !isInRange(a, b)
+          ) {
+            return false;
+          }
+
+          i += 2;
+          joker = true;
+        }
+      }
+    }
+    return true;
+  }).length;
+  console.log("[LS] -> main.ts:66 -> resPart2: ", resPart2);
+}
+
+function isInRange(a: number, b: number) {
+  const range = [1, 2, 3];
+  return range.includes(Math.abs(a - b));
 }
