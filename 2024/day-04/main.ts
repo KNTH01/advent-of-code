482ef02f2b7367b53c14635addb94ae2ed0ee7b9
Kevin Nguyen <knth@hey.com>
2024-12-04 15:25:55 +0100

    feat: part2 day-04

--- 2024/day-04/main.ts
+++ 2024/day-04/main.ts
@@ -1,7 +1,4 @@
-import { assertEquals } from "@std/assert";
-
 if (import.meta.main) {
-  // const inputFile = "input-base.txt";
   const inputFile = "input.txt";
   const text = await Deno.readTextFile(inputFile);
 
@@ -9,6 +6,9 @@ if (import.meta.main) {
 
   const res1 = part1(grid);
   console.log("[LS] -> main.ts:20 -> res1: ", res1);
+
+  const res2 = part2(grid);
+  console.log("[LS] -> main.ts:13 -> res2: ", res2);
 }
 
 function part1(grid: Grid) {
@@ -36,7 +36,41 @@ function part1(grid: Grid) {
   return counts.reduce((acc, curr) => acc + curr, 0);
 }
 
-function part2(grid: Grid) {}
+function part2(grid: Grid) {
+  const aPositions = findPositionsForChar(grid, "A");
+
+  const counts = aPositions
+    .map(({ x, y }) => {
+      const conf1 =
+        grid[y + 1]?.[x + 1] === "M" &&
+        grid[y + 1]?.[x - 1] === "M" &&
+        grid[y - 1]?.[x + 1] === "S" &&
+        grid[y - 1]?.[x - 1] === "S";
+
+      const conf2 =
+        grid[y + 1]?.[x + 1] === "S" &&
+        grid[y + 1]?.[x - 1] === "S" &&
+        grid[y - 1]?.[x + 1] === "M" &&
+        grid[y - 1]?.[x - 1] === "M";
+
+      const conf3 =
+        grid[y + 1]?.[x + 1] === "M" &&
+        grid[y + 1]?.[x - 1] === "S" &&
+        grid[y - 1]?.[x + 1] === "M" &&
+        grid[y - 1]?.[x - 1] === "S";
+
+      const conf4 =
+        grid[y + 1]?.[x + 1] === "S" &&
+        grid[y + 1]?.[x - 1] === "M" &&
+        grid[y - 1]?.[x + 1] === "S" &&
+        grid[y - 1]?.[x - 1] === "M";
+
+      return conf1 || conf2 || conf3 || conf4;
+    })
+    .filter(Boolean);
+
+  return counts.length;
+}
 
 type Grid = string[][];
 
